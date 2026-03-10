3eba8652342359026a2c4a29a3a85e233ec3a6cf
VincentDucrot <vincent.ducrot@cis-france.net>
2026-03-10 16:14:20 +0100

    Add unit test for verifying GenericCardApiProperties version

---
+++ src/test/java/org/eclipse/keypop/genericcard/GenericCardApiPropertiesTest.java
@@ -0,0 +1,45 @@
+/* **************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association https://calypsonet.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * MIT License which is available at https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ ************************************************************************************** */
+package org.eclipse.keypop.genericcard;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GenericCardApiPropertiesTest {
+
+  private static String libVersion;
+
+  @BeforeAll
+  public static void beforeAll() throws Exception {
+    InputStream inputStream = new FileInputStream("gradle.properties");
+    try {
+      Properties properties = new Properties();
+      properties.load(inputStream);
+      libVersion = properties.getProperty("version");
+    } finally {
+      inputStream.close();
+    }
+  }
+
+  @Test
+  public void versionIsCorrectlyWritten() {
+    String apiVersion = GenericCardApiProperties.VERSION;
+    assertThat(apiVersion).matches("\\d+\\.\\d+");
+    assertThat(libVersion).startsWith(apiVersion);
+  }
+}
